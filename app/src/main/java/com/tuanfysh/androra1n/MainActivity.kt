51b9d1d3d5855dbeae4a900958db7221da9a29ff
tunafysh <hanansmani@gmail.com>
2024-10-04 11:12:54 +0200

    Initial commit

---
+++ app/src/main/java/com/tuanfysh/androra1n/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.tuanfysh.androra1n
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tuanfysh.androra1n.ui.theme.Androra1nTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Androra1nTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Greeting(
+                        name = "world",
+                        modifier = Modifier
+                    )
+                    ShowHideBoxExample()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowHideBoxExample() {
+    var isVisible by remember { mutableStateOf(false)}
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton (onClick = { isVisible = !isVisible }) {
+                Icon(Icons.Default.Add, contentDescription = "Toggle Box")
+            }
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            contentAlignment = Alignment.Center
+        ) {
+            if (isVisible) {
+                Box(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.primary),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Hello, world!", color = MaterialTheme.colorScheme.onPrimary)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+}
+
+fun idk(content: String): String {
+    return "Hello $content!"
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Androra1nTheme {
+        FloatingActionButton(onClick = {}) { }
+        Greeting("Test")
+    }
+}
